468f0ca56f9b95c2bf9988ea6901f785f84fa630
Banni <bannid@gmail.com>
2023-05-25 15:28:22 +1000

    Initial commit

---
+++ InvestmentAppProd/Data/IInvestmentRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using InvestmentAppProd.Models;
+
+namespace InvestmentAppProd.Data
+{
+    public interface IInvestmentRepository
+    {
+
+        public List<Investment> GetInvestments();
+        public Investment GetInvestment();
+        public bool InsertInvestment();
+        public bool UpdateInvestment();
+        public bool DeleteInvestment();
+
+    }
+}
